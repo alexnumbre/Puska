32ada645f328f19acd9b7272275363e5be0ae467
Алексей <1704alex2004@gmail.com>
2022-04-04 19:47:31 +0300

    001

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+screen=pygame.display.set_mode([600,600])
+f=pygame.image.load("mysprites/dirt.jpg")
+fheight=f.get_height()
+screen.blit(f,[0,600-fheight])
+pygame.display.flip()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+while True:
+   events= pygame.event.get()
+   for event in events:
+       if event.type == pygame.QUIT:
+           exit()
